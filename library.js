88ac3887214c8cd734e83516076ca86182c67e09
gl0bix <d.schellenberg2109@gmail.com>
2021-06-14 15:27:56 +0200

    add and display books

--- library.js
+++ library.js
@@ -8,11 +8,19 @@ const pagesInput = document.querySelector('#pages');
 const readInput = document.querySelector('#read');
 
 const submit = document.querySelector('#submit');
+const save = document.querySelector('#save');
+const refresh = document.querySelector('#refresh');
 
-let list = document.querySelector('#libraryList')
+let table = document.querySelector('#library-table');
+
+let ID = 0;
 
 const Book = {
+
+    //include shared variable for id
+
     init: function (title, author, pages, read) {
+        this.id = ID++;
         this.title = title;
         this.author = author;
         this.pages = pages;
@@ -40,11 +48,38 @@ function addBook(e) {
 }
 
 function displayBooks() {
-    let result = "";
+    //fix double entries
+    let result = `
+    <tr>
+        <th>ID</th>
+        <th>Title</th>
+        <th>Author</th>
+        <th>Pages</th>
+        <th>Read?</th>
+    </tr>`;
     myLibrary.forEach((el) => {
-        result += `<li> ${el.title}, ${el.author} ${el.pages}, ${el.read ? 'red it' : 'not read yet'}`
+        result += `
+        <tr data-id="${el.id}"> 
+            <td>${el.id}</td>
+            <td>${el.title}</td> 
+            <td>${el.author}</td> 
+            <td>${el.pages}</td> 
+            <td><input type="checkbox" ${el.read}? checked: ></td>
+        </tr>`
     })
-    list.innerHTML = result;
+    table.innerHTML = result;
 }
 
+function saveLibrary(){
+    // save books in online storage
+}
+
+
 form.addEventListener('submit', addBook);
+refresh.addEventListener('click', displayBooks);
+save.addEventListener('click', saveLibrary);
+
+myLibrary.push(Object.create(Book).init("The Hobbit", "J.R.R Tolkien", 200, true));
+myLibrary.push(Object.create(Book).init("Dune", "Frank Herbert", 300, false));
+myLibrary.push(Object.create(Book).init("Eragon", "Christopher Paolini", 600, true));
+displayBooks();
