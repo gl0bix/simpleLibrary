3235634ef0107e5f4976ab29002e3f7e1e54d34a
gl0bix <d.schellenberg2109@gmail.com>
2021-06-27 16:02:37 +0200

    realized toggle option for read status and updated table creation

--- library.js
+++ library.js
@@ -11,14 +11,11 @@ const submit = document.querySelector('#submit');
 const save = document.querySelector('#save');
 const refresh = document.querySelector('#refresh');
 
-let table = document.querySelector('#library-table');
+const libraryDiv = document.querySelector('#library')
 
 let ID = 0;
 
 const Book = {
-
-    //include shared variable for id
-
     init: function (title, author, pages, read) {
         this.id = ID++;
         this.title = title;
@@ -29,6 +26,9 @@ const Book = {
     },
     info: function () {
         console.log(`${this.title} by ${this.author}, ${this.pages} pages, ${this.read ? 'red it' : 'not read yet'}`);
+    },
+    toggleRead: function(){
+        this.read = !this.read;
     }
 }
 
@@ -38,7 +38,7 @@ function addBook(e) {
         titleInput.value,
         authorInput.value,
         pagesInput.value,
-        readInput.value
+        readInput.value === 'on' ? true : false
     );
 
     myLibrary.push(newBook);
@@ -47,27 +47,77 @@ function addBook(e) {
     displayBooks();
 }
 
+// TODO: rather use createElement
+
 function displayBooks() {
-    //fix double entries
-    let result = `
-    <tr>
-        <th>ID</th>
-        <th>Title</th>
-        <th>Author</th>
-        <th>Pages</th>
-        <th>Read?</th>
-    </tr>`;
+    
+    while(libraryDiv.firstChild) libraryDiv.removeChild(libraryDiv.firstChild);
+
+    const table = document.createElement("table");
+    table.setAttribute("class", "library-table");
+    table.setAttribute("class", "library-table");
+    table.setAttribute("id", "library-table");
+
+    //define table head
+    const tableHead = document.createElement("tr");
+    const headID = document.createElement("th");
+    headID.textContent = 'ID';
+    const headTitle = document.createElement("th");
+    headTitle.textContent = 'Title';
+    const headAuthor = document.createElement("th");
+    headAuthor.textContent = 'Author';
+    const headPages = document.createElement("th");
+    headPages.textContent = 'Pages';
+    const headRead = document.createElement("th");
+    headRead.textContent = 'Read?';
+
+    tableHead.appendChild(headID);
+    tableHead.appendChild(headTitle);
+    tableHead.appendChild(headAuthor);
+    tableHead.appendChild(headPages);
+    tableHead.appendChild(headRead);
+    table.appendChild(tableHead);
+
+    //define table rows
+
     myLibrary.forEach((el) => {
-        result += `
-        <tr data-id="${el.id}"> 
-            <td>${el.id}</td>
-            <td>${el.title}</td> 
-            <td>${el.author}</td> 
-            <td>${el.pages}</td> 
-            <td><input type="checkbox" ${el.read}? checked: ></td>
-        </tr>`
-    })
-    table.innerHTML = result;
+        let bookRow = document.createElement("tr");
+        bookRow.setAttribute("data-id", `${el.id}`);
+
+        const rowID = document.createElement("td");
+        rowID.textContent =`${el.id}`;
+        const rowTitle = document.createElement("td");
+        rowTitle.textContent = `${el.title}`;
+        const rowAuthor = document.createElement("td");
+        rowAuthor.textContent = `${el.author}`;
+        const rowPages = document.createElement("td");
+        rowPages.textContent = `${el.pages}`;
+        const rowRead = document.createElement("td");
+        const rowReadButton = document.createElement("button");
+        rowReadButton.textContent = `${el.read}`;
+        rowReadButton.setAttribute("class", "button read-button");
+        rowReadButton.setAttribute("data-id", `${el.id}`);
+        rowRead.appendChild(rowReadButton);
+
+        bookRow.appendChild(rowID);
+        bookRow.appendChild(rowTitle);
+        bookRow.appendChild(rowAuthor);
+        bookRow.appendChild(rowPages);
+        bookRow.appendChild(rowRead);
+
+        table.appendChild(bookRow);
+    });
+
+    libraryDiv.appendChild(table);
+
+    let readButtons = document.querySelectorAll(".read-button");
+
+    readButtons.forEach((e) => {
+        e.addEventListener("click", () => {
+            myLibrary.find(book => book.id === parseInt(e.getAttribute("data-id"))).toggleRead();
+            displayBooks();
+        })
+    });
 }
 
 function saveLibrary(){
